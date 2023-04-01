d0a95a1f65e6fc30e73833e9b34fead957845c3b
FantasticSukhi <122316561+FantasticSukhi@users.noreply.github.com>
2023-04-02 02:35:06 +0530

    Update activevc.py

--- FallenMusic/Modules/activevc.py
+++ FallenMusic/Modules/activevc.py
@@ -1,25 +1,3 @@
-# MIT License
-#
-# Copyright (c) 2023 AnonymousX1025
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -30,7 +8,7 @@ from FallenMusic.Helpers.inline import close_key
 
 @app.on_message(filters.command("activevc") & SUDOERS)
 async def activevc(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ʟɪsᴛ...")
+    mystic = await message.reply_text("»»» 🄶🄴🅃🅃🄸🄽🄶 🄰🄲🅃🄸🅅🄴 🅅🄾🄸🄲🄴🄲🄷🄰🅃🅂 🄻🄸🅂🅃")
     chats = await get_active_chats()
     text = ""
     j = 0
@@ -46,10 +24,10 @@ async def activevc(_, message: Message):
             text += f"<b>{j + 1}. {title}</b> [`{chat}`]\n"
         j += 1
     if not text:
-        await mystic.edit_text("ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄʙᴏᴛ...")
+        await mystic.edit_text("»»» 🄽🄾 🄰🄲🅃🄸🅅🄴 🅅🄾🄸🄲🄴🄲🄷🄰🅃🅂 🄾🄽 🄼🅄🅂🄸🄲🄱🄾🅃")
     else:
         await mystic.edit_text(
-            f"**ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄ ʙᴏᴛ :**\n\n{text}",
+            f"**🄻🄸🅂🅃 🄾🄵 🄲🅄🅁🅁🄴🄽🅃🄻🅈 🄰🄲🅃🄸🅅🄴 🅅🄾🄸🄲🄴🄲🄷🄰🅃🅂 🄾🄽 🄼🅄🅂🄸🄲🄱🄾🅃 :**\n\n{text}",
             reply_markup=close_key,
             disable_web_page_preview=True,
         )
